0bd19405acdc0352d7178735171ea2df2d1c63ea
Ben Draper <54486270+bendraper00@users.noreply.github.com>
2022-01-26 20:05:51 -0500

    added global vars config file

---
+++ config.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+//Pins Assignments
+static const FRONT_ENCODER_A = 2;
+static const FRONT_ENCODER_B = 51;
+static const FRONT_DRIVE_EN = 10;
+static const FRONT_DRIVE_IN1 = 53;
+static const FRONT_DRIVE_IN2 = 52;
+static const FRONT_DRIVE_SERVO = 9;
+
+//static const REAR_ENCODER_A = 0;
+//static const REAR_ENCODER_B = 0;
+//static const REAR_DRIVE_EN = 0;
+//static const REAR_DRIVE_IN1 = 0;
+//static const REAR_DRIVE_IN2 = 0;
+//static const REAR_DRIVE_SERVO = 0;
+
+//-------------------------------------------//
+
+//Other Variables
+static const MAX_DRIVE_SPEED = 255;
+static const;
+static const;
+static const;
+static const;
+#endif
