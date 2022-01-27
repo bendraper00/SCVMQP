8a0bdee997b48c7afc8b2854fa6e430be82d591d
Ben Draper <54486270+bendraper00@users.noreply.github.com>
2022-01-26 20:05:36 -0500

    added global vars config file

--- SCVMQP.ino
+++ SCVMQP.ino
@@ -1,18 +1,16 @@
 #include <Arduino.h>
+#include "config.h"
 #include <stdint.h>
 #include "src\classes\DriveModule\DriveModule.h"
 #include "src\classes\Sensors\Sensors.h"
 
-//PIN DEFINITIONS
-uint8_t chA = 2;
-uint8_t chB = 51;
-const unsigned int EN = 10;
-const unsigned int IN1 = 53;
-const unsigned int IN2 = 52;
-uint8_t servoPin = 9;
-
 //Objects
-DriveModule mod(EN, IN1, IN2, chA, chB, servoPin);
+DriveModule mod(FRONT_DRIVE_EN, 
+                FRONT_DRIVE_IN1, 
+                FRONT_DRIVE_IN2, 
+                FRONT_ENCODER_A, 
+                FRONT_ENCODER_B, 
+                FRONT_DRIVE_SERVO);
 Sensors sens;
 
 //Temp Variables for testing
