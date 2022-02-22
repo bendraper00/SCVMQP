b2d99d65d0e6738fb90c8ff741d7207bff0c62f2
Ben Draper <54486270+bendraper00@users.noreply.github.com>
2022-02-22 10:16:50 -0500

    append to previous commit

--- src/classes/Sensors/Sensors.cpp
+++ src/classes/Sensors/Sensors.cpp
@@ -41,60 +41,20 @@ void Sensors::init(){
     this->sensB.setAddress(LOX2_ADDRESS);
 }
 
-void Sensors::printRange(){
-    uint16_t mask = 1;
-    for(uint8_t i = 0; i < TOF_SENSOR_COUNT; i++) {
-        bool range_complete = false;
-        if (sensors_pending & mask) {
-            range_complete = this->sens[i]->isRangeComplete();
-            if(range_complete) {
-                sensor_ranges[i] = this->sens[i]->readRangeResult();
-                sensor_status[i] = this->sens[i]->readRangeStatus();
-                sensors_pending ^= mask;
-            }
-        }
-
-        mask <<= 1; // setup to test next one
-    }
-    // See if we have all of our sensors read OK
-    uint32_t delta_time = millis() - sensor_last_cycle_time;
-    if (!sensors_pending || (delta_time > 1000)) {
-        Serial.print(delta_time, DEC);
-        Serial.print("(");
-        Serial.print(sensors_pending, HEX);
-        Serial.print(")");
-        mask = 1;
-        for (uint8_t i = 0; i < TOF_SENSOR_COUNT; i++) {
-            Serial.print(" : ");
-            if (sensors_pending & mask) Serial.print("TTT");  // show timeout in this one
-            else if (sensor_status[i] == VL6180X_ERROR_NONE) Serial.print(sensor_ranges[i], DEC);
-            else {
-                Serial.print("#");
-                Serial.print(sensor_status[i], DEC);
-            }
-        }
-    // setup for next pass
-    Serial.println();
-    sensor_last_cycle_time = millis();
-    sensors_pending = ALL_SENSORS_PENDING;
-  }
-}
-
-uint16_t Sensors::getRange(){
-    return 1;
-}
-
-void Sensors::startRanging(){
-    for (uint8_t i = 0; i < TOF_SENSOR_COUNT; i++){
-        this->sens[i]->startRangeContinuous(TOF_PERIOD);
-    }
-    sensors_pending = ALL_SENSORS_PENDING;
-    sensor_last_cycle_time = millis();
-}
-
-void Sensors::stopRanging() {
+uint8_t Sensors::getRangeMagnitude(){
+    uint8_t magnitude = 0;
     for (uint8_t i = 0; i < TOF_SENSOR_COUNT; i++) {
-        this->sens[i]->stopRangeContinuous();
+        magnitude += this->sens[i]->readRange();
     }
-    delay(10); // give time for it to complete.
+    return magnitude/TOF_SENSOR_COUNT;
 }
+
+int16_t Sensors::getDifference(){
+    for (uint8_t i = 0; i < TOF_SENSOR_COUNT; i++) {
+        ranges[i] = sens[i]->readRange();
+    }
+    return ranges[0]-ranges[1]; //Application specific function
+}
+
+
+
