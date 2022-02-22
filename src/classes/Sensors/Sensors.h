6e87e4ecf9783de92fa4646fde9daa600755abbc
Ben Draper <54486270+bendraper00@users.noreply.github.com>
2022-02-22 10:16:38 -0500

    resolved issues with sensors

--- src/classes/Sensors/Sensors.h
+++ src/classes/Sensors/Sensors.h
@@ -9,16 +9,12 @@ class Sensors{
     public:
         Sensors();
         void init();
-        void printRange();
-        uint16_t getRange();
-        void startRanging();
-        void stopRanging();
 
-        const uint16_t  ALL_SENSORS_PENDING = ((1 << TOF_SENSOR_COUNT) - 1);
-        uint16_t sensors_pending = ALL_SENSORS_PENDING;
-        uint32_t sensor_last_cycle_time;
         uint8_t sensor_ranges[TOF_SENSOR_COUNT];
         uint8_t sensor_status[TOF_SENSOR_COUNT];
+        uint8_t getRangeMagnitude();
+        int16_t getDifference();
+        uint8_t ranges[TOF_SENSOR_COUNT];
 
     private:
         Adafruit_VL6180X sensA;
