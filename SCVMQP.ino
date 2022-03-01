ce9cf05ac6af8a98ad2619fd9ae1a67f11371e5a
Draper, Benjamin John (894699051) <bjdraper@wpi.edu>
2022-03-01 17:13:00 -0500

    updated demo

--- SCVMQP.ino
+++ SCVMQP.ino
@@ -11,7 +11,7 @@ IRrecv ir(IR_REMOTE_SIGNAL);
 
 //Temp Variables for testing
 uint32_t prevt = 0;
-enum command{RAISEFRONT, LOWERFRONT, RAISEREAR, LOWERREAR, FORWARD, BACKWARD, LEFTSKID, RIGHTSKID, LATERAL, LEFT, RIGHT, STOP};
+enum command{RAISEMID, LOWERMID, RAISEFRONT, LOWERFRONT, RAISEREAR, LOWERREAR, FORWARD, BACKWARD, LEFTSKID, RIGHTSKID, LATERAL, LEFT, RIGHT, STOP};
 command state = STOP;
 command prevState = STOP;
 bool lateral = false;
@@ -37,21 +37,39 @@ void loop()
     else if(comm == 25){state = LOWERFRONT;}
     else if(comm == 12){state = LOWERREAR;}
     else if(comm == 24){state = RAISEREAR;}
-    else if(comm == 70 && lateral == false){state = FORWARD;}
-    else if(comm == 68 && lateral == false){state = LEFTSKID;}
-    else if(comm == 21 && lateral == false){state = BACKWARD;}
-    else if(comm == 67 && lateral == false){state = RIGHTSKID;}
+    else if(comm == 13){state = RAISEMID;}
+    else if(comm == 94){state = LOWERMID;}
+    else if(comm == 70 && lateral == false){state = LEFTSKID;}
+    else if(comm == 68 && lateral == false){state = BACKWARD;}
+    else if(comm == 21 && lateral == false){state = RIGHTSKID;}
+    else if(comm == 67 && lateral == false){state = FORWARD;}
     else if(comm == 64 && prevState != LATERAL){state = LATERAL;}
-    else if(comm == 68 && lateral == true){state = LEFT;}
-    else if(comm == 21 && lateral == true){state = STOP;}
-    else if(comm == 70 && lateral == true){state = STOP;}
-    else if(comm == 67 && lateral == true){state = RIGHT;}
+    else if(comm == 21 && lateral == true){state = RIGHT;}
+    else if(comm == 68 && lateral == true){state = STOP;}
+    else if(comm == 67 && lateral == true){state = STOP;}
+    else if(comm == 70 && lateral == true){state = LEFT;}
     else{state = STOP;}
     Serial.println(comm);
     ir.resume();
   }
 
   switch(state){
+    case RAISEMID:
+      bot.scissors->lowerFront(255);
+      bot.scissors->lowerRear(255);
+      bot.frontDrive->driveSpeed(0);
+      bot.rearDrive->driveSpeed(0);
+      prevState = RAISEMID;
+      break;
+    
+    case LOWERMID:
+      bot.scissors->raiseFront(255);
+      bot.scissors->raiseRear(255);
+      bot.frontDrive->driveSpeed(0);
+      bot.rearDrive->driveSpeed(0);
+      prevState = LOWERMID;
+      break;
+
     case RAISEFRONT:
       bot.scissors->raiseFront(255);
       bot.frontDrive->driveSpeed(0);
@@ -85,32 +103,32 @@ void loop()
       break;
 
     case FORWARD:
-      bot.frontDrive->driveSpeed(120);
-      bot.rearDrive->driveSpeed(120);
+      bot.frontDrive->driveSpeed(DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = FORWARD;
       break;
 
     case BACKWARD:
-      bot.frontDrive->driveSpeed(-120);
-      bot.rearDrive->driveSpeed(-120);
+      bot.frontDrive->driveSpeed(-DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(-DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = BACKWARD;
       break;
 
     case LEFTSKID:
-      bot.frontDrive->driveSpeed(120);
-      bot.rearDrive->driveSpeed(-120);
+      bot.frontDrive->driveSpeed(DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(-DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = LEFTSKID;
       break;
 
     case RIGHTSKID:
-      bot.frontDrive->driveSpeed(-120);
-      bot.rearDrive->driveSpeed(120);
+      bot.frontDrive->driveSpeed(-DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = RIGHTSKID;
@@ -135,16 +153,16 @@ void loop()
       break;
 
     case LEFT:
-      bot.frontDrive->driveSpeed(-120);
-      bot.rearDrive->driveSpeed(-120);
+      bot.frontDrive->driveSpeed(-DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(-DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = LEFT;
       break;
 
     case RIGHT:
-      bot.frontDrive->driveSpeed(120);
-      bot.rearDrive->driveSpeed(120);
+      bot.frontDrive->driveSpeed(DEMO_DRIVE_SPEED);
+      bot.rearDrive->driveSpeed(DEMO_DRIVE_SPEED);
       bot.scissors->frontSpeed(0);
       bot.scissors->rearSpeed(0);
       prevState = RIGHT;
